387691be8ec91ce57fe15a71d8974af0833e48dd
Patrick Rohr <patrickrohr92@gmail.com>
2020-04-14 22:53:17 +0200

    Adding Some Generic Device Function Structure
    Still thinking about what exactly this will look like.

--- device.h
+++ device.h
@@ -7,9 +7,14 @@
 #include <linux/netdevice.h>
 #include <net/cfg80211.h>
 
+struct pr_device_ops {
+    void (*load_firmware)(void); /* Loads the firmware */
+};
+
 struct pr_device_handle {
     struct wiphy* wiphy;
     struct net_device* netdev;
+    struct device_ops* device_ops;
     struct wireless_dev wdev;
 };
 
